973ee9f502f4dcb94a2cdb3dbb797ea183e87fde
wangpeng <wangpeng@yunionyun.com>
2019-07-22 12:43:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ $lengbandGitIo = '/Users/wangpeng/learn/code/blog/me/lengband.github.io'
 git add .
 git commit -am 'update'
 git push
-gitbook build && mv -f ./_book $lengbandGitIo
+gitbook build && cp -R ./_book/* $lengbandGitIo
 cd $lengbandGitIo
 git add .
 git commit -am 'update'
